f4de76b82318ac682c43cdc6c5547e6f0fe40397
papa <visidoor@yandex.ru>
2026-07-11 14:43:10 +0500

    gitcore: implement directory detection

--- gitcore/lib/filesystem.sh
+++ gitcore/lib/filesystem.sh
@@ -13,3 +13,21 @@ gitcore_directory_entries()
 {
     ls -A -- "$1"
 }
+
+###############################################################################
+# gitcore_is_directory
+#
+# Determine whether a path refers to a directory.
+#
+# Usage:
+#     gitcore_is_directory PATH
+#
+# Exit status:
+#     0  Path is a directory.
+#     1  Path is not a directory.
+###############################################################################
+
+gitcore_is_directory()
+{
+    [ -d "$1" ]
+}
