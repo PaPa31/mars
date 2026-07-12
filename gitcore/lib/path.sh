6c243962b68d6da2f9fde35c97f01586960693b8
papa <visidoor@yandex.ru>
2026-07-12 14:02:28 +0500

    gitcore: implement path dirname observation
    why:
    Provide a reusable observation for obtaining the directory portion
    of a pathname.
    Complement the existing basename observation.
    
    next:
    Continue expanding reusable path observations.

--- gitcore/lib/path.sh
+++ gitcore/lib/path.sh
@@ -13,3 +13,17 @@ gitcore_path_basename()
 {
     basename -- "$1"
 }
+
+###############################################################################
+# gitcore_path_dirname
+#
+# Return all but the final pathname component of a path.
+#
+# Usage:
+#     gitcore_path_dirname PATH
+###############################################################################
+
+gitcore_path_dirname()
+{
+    dirname -- "$1"
+}
