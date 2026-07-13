2a95d228db4b6914cad4b8ab8a44dce32a2e8bf2
papa <visidoor@yandex.ru>
2026-07-13 13:11:24 +0500

    gitcore: implement detached HEAD detection
    why:
    Provide a reusable observation for determining
    whether HEAD is detached from a branch.
    
    next:
    Compose detached HEAD observations into reports.

--- gitcore/lib/repository.sh
+++ gitcore/lib/repository.sh
@@ -20,6 +20,26 @@ gitcore_head()
     git rev-parse HEAD
 }
 
+###############################################################################
+# gitcore_head_is_detached
+#
+# Determine whether HEAD is detached.
+#
+# Exit status:
+#     0  HEAD is detached.
+#     1  HEAD is attached to a branch.
+###############################################################################
+
+gitcore_head_is_detached()
+{
+    if git symbolic-ref --quiet HEAD >/dev/null 2>&1
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
 gitcore_is_repository_clean()
 {
     if [ -n "$(git status --porcelain)" ]
