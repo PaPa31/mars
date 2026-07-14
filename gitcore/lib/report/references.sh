84a794eabca3277732a48cfa4673f395006e70b2
papa <visidoor@yandex.ru>
2026-07-14 20:03:16 +0500

    gitcore: report upstream branch

--- gitcore/lib/report/references.sh
+++ gitcore/lib/report/references.sh
@@ -12,4 +12,7 @@ gitcore_report_references()
 
     gitcore_print_heading "Repository description"
     gitcore_print_indented "$(gitcore_repository_description)"
+
+    gitcore_print_heading "Upstream branch"
+    gitcore_print_indented "$(gitcore_upstream_branch)"
 }
