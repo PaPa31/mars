d4c870d4b5adbbdf8ada2af09504879c44a475a3
papa <visidoor@yandex.ru>
2026-07-14 18:51:21 +0500

    gitcore: report bare repository state

--- gitcore/lib/report/repository_state.sh
+++ gitcore/lib/report/repository_state.sh
@@ -3,6 +3,10 @@
 gitcore_report_repository_state()
 {
     gitcore_print_section "Repository state" "----------------"
+
     gitcore_print_heading "Repository clean"
     gitcore_print_indented "$(gitcore_is_repository_clean)"
+
+    gitcore_print_heading "Bare repository"
+    gitcore_print_indented "$(gitcore_repository_is_bare)"
 }
